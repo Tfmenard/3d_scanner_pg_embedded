0feec9bf3473bc31b859d478338b9e1babc7ddd3
gabrielcv95 <gabriel.chootong@mail.mcgill.ca>
2018-03-05 18:41:04 -0500

    parsing and main code for base and Y motors and encoders, with fixed bugs for motor and control loop classes

--- pos_ctrl_loop_msg_raw/pos_ctrl_loop_msg_raw.ino
+++ pos_ctrl_loop_msg_raw/pos_ctrl_loop_msg_raw.ino
@@ -1,3 +1,4 @@
+//This code mainly shows how to split the string from pc input into different strings to be used by control loop and motor.
 #include <Encoder.h>
 #include <PID_v1.h>
 
@@ -9,6 +10,9 @@ double Kp = 5, Ki = 0, Kd = 0;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 long oldPosition  = -999;
 bool pidComputed = false;
+
+
+String PC_input, command, id, value;
 
 void setup() {
   Serial.begin(115200);
@@ -45,8 +49,22 @@ void loop() {
     Serial.print("Go forward");
   }
   while (Serial.available() > 0) {
-    Setpoint = Serial.parseInt();
+    PC_input = Serial.readStringUntil('\n');
+    //splitting PC_input:
+    command = getValue(PC_input, ',', 0);
+    id = getValue(PC_input, ',', 1);
+    value = getValue(PC_input, ',', 2);
+
+    Setpoint = value.toDouble();
+    //
+    //Input for motor:
+    
+    
+//    Setpoint = Serial.parseInt();
+//    Serial.print("New setpoint:" + Setpoint);
   }
+      Serial.print(command + ' ' + id + ' ' + value);
+
     pidComputed = myPID.Compute();
 
     if (Output > 0) {
@@ -72,4 +90,19 @@ void loop() {
     Serial.print(Input);
     Serial.print(", Out: ");
     Serial.println(Output);
+}
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
