bae2f36afaa520a290f6e175e7bc4d1562a8fe2d
gabrielcv95 <gabriel.chootong@mail.mcgill.ca>
2018-03-23 22:45:26 -0400

    change address for personal computer -gabriel

--- base_Y_main/base_Y_main.ino
+++ base_Y_main/base_Y_main.ino
@@ -9,9 +9,8 @@
 
 */
 
-
 // Change this path to include libraries
-#define PROJECT_ROOT C:\Users\Tfmenard\Documents\GitHub\3d_scanner_pg_embedded\libraries
+#define PROJECT_ROOT C:\Users\Gabriel\Desktop\Deberes McGill\DP2\Github\3d_scanner_pg_embedded\libraries
 #define TO_STRING(s) #s
 #define ABSOLUTE_PATH(root, relative_path) TO_STRING(root\relative_path)
 #define RELATIVE_PATH(library) ABSOLUTE_PATH(PROJECT_ROOT, library)
